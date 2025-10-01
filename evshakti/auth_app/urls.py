c73a621563a8c49a607ca3cfa84f4b9124219166
runankpatil08 <runankpatil08@gmail.com>
2025-10-01 17:18:19 +0530

    change some UI

--- evshakti/auth_app/urls.py
+++ evshakti/auth_app/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
 from .views import register, login_view,logout_view
-
+from django.contrib.auth import views as auth_views
 urlpatterns=[
     path("register/",register,name='register'),
     path('login/',login_view,name='login'),
-    path('logout/',logout_view,name='logout')
+    path('logout/',logout_view,name='logout'),
+# Password Reset URLs
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name='auth_app/password_reset.html'), name='reset_password'),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='auth_app/password_reset_sent.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='auth_app/password_reset_form.html'), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='auth_app/password_reset_done.html'), name='password_reset_complete'),
 
 ]
